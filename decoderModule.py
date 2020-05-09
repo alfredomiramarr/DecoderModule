dadc6358bf8aee2711135029369aa0a4300d0593
alfredomiramarr <alfredomiramarr@users.noreply.github.com>
2020-05-08 20:36:58 -0500

    Versión Actualizada
    Versión final. Examen Tercer Parcial

--- decoderModule.py
+++ decoderModule.py
@@ -8,22 +8,35 @@ def decodes(code,advancekey):
 	decoded_string = ""
 	code_upper = code.upper()
 	decoded_list = split(code)
-
+    
 	list_alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", 
 	"J", "K","L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-	
+    
 	list_number = ["0", "1" , "2", "3", "4", "5", "6", "7", "8", "9"]
-	
+
 	print ("original chain: " + code)
 	while length_iterator < len(code_upper):
 		
 		char_to_explore = decoded_list[length_iterator]
 
 		if char_to_explore.isalpha():
-
 			index_list = list_alphabet.index(char_to_explore)
 			decode_index = index_list + int(advancekey)
-			decoded_string = decoded_string + list_alphabet[decode_index]
+			if decode_index > 25:
+				decode_index = decode_index-26
+				decoded_string = decoded_string + list_alphabet[decode_index]
+			else:
+				decoded_string = decoded_string + list_alphabet[decode_index]
+
+		if char_to_explore.isdigit():
+			index_list = list_number.index(char_to_explore)
+			decode_index = index_list + int(advancekey)
+			if decode_index > 9:
+				decode_index = decode_index-10
+				decoded_string = decoded_string + list_number[decode_index]
+			else:
+				decoded_string = decoded_string + list_number[decode_index]
+			
 		
 		length_iterator = length_iterator + 1
 
@@ -33,10 +46,12 @@ def main():
 	
 	print ("-------- RebelAllianceDecoderModule ---------")
 	
-	inputx = input ("R2 insert the key to decode: ")
+	inputx = input("R2 insert the key to decode: ")
+	inputM = inputx.upper()
 	input_advance = input ("R2 insert the advance key to generate the decode result: ")
-	decodes(inputx,input_advance)
+	decodes(inputM,input_advance)
 
 if __name__ == "__main__":
 	main()
+
 
